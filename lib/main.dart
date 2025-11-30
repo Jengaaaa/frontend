e1fd368d48e3c65fbeaac5a008d5fc82273c305d
mercury0925 <164193735+mercury0925@users.noreply.github.com>
2025-11-30 21:58:25 +0900

    connect backend (login/signup)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/features/screens/welcome_screen.dart';
 import 'package:frontend/features/survey/survey_job_screen.dart';
 import 'package:frontend/features/survey/survey_info_screen.dart';
 import 'package:frontend/features/survey/survey_face_screen.dart';
 import 'package:frontend/features/survey/survey_camera_screen.dart';
+import 'package:frontend/features/screens/welcome_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,8 +36,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      // home: const WelcomeScreen(),
-      home: const SurveyJobScreen(),
+      home: const WelcomeScreen(),
+      // home: const SurveyJobScreen(),/
       // home: const HomeScreen(),
       routes: {
         '/survey-camera': (context) => const SurveyCameraScreen(),
